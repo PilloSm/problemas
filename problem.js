cd734ca4f1eed1683b1630294c20f4f51edddd11
PilloSm <corzo.arguello.luis@gmail.com>
2023-03-06 23:57:47 -0600

    fi

--- problem.js
+++ problem.js
@@ -1,25 +1,25 @@
 function interes()
 {
-    let dinero=Problemas.getElementById("dinero").value;
+    let dinero=document.getElementById("dinero").value;
     let capregex=/^\d+(\.\d+)?$/; 
     let resultado;
     if (capregex.test(dinero))
     {
-        resultado=dinero*0.2
-        Problemas.getElementById("res").value=resultado;
+        resultado=dinero*1.02
+        document.getElementById("res").value=resultado;
     }
     else
     {
         alert("Valor incorrecto")
     } 
 }
-function sueldo()
+function sueld()
 {
     let checar=/^\d+(\.\d+)?$/;  
-    let venta1=Problemas.getElementById("venta1").value;
-    let venta2=Problemas.getElementById("venta2").value;
-    let venta3=Problemas.getElementById("venta3").value;
-    let sueldo=Problemas.getElementById("sueldo").value;
+    let venta1=document.getElementById("venta1").value;
+    let venta2=document.getElementById("venta2").value;
+    let venta3=document.getElementById("venta3").value;
+    let sueldo=document.getElementById("sueldo").value;
 
     if (checar.test(venta1))
     {
@@ -31,9 +31,9 @@ function sueldo()
             {
               let comision=(parseFloat(venta1)+parseFloat(venta2)+parseFloat(venta3))*0.1;
               let sueldototal=parseFloat(sueldo)+parseFloat(comision);
-              Problemas.getElementById("sueldoR").value=sueldo;
-              Problemas.getElementById("comR").value=comision;
-              Problemas.getElementById("sueldoTR").value=sueldototal;
+              document.getElementById("sueldoR").value=sueldo;
+              document.getElementById("comR").value=comision;
+              document.getElementById("sueldoTR").value=sueldototal;
             }
             else
             {
@@ -58,13 +58,12 @@ function sueldo()
 }
 function descuento()
 {
-  let compra=Problemas.getElementById("compra").value;
-  let compraregex=/^\d+(\.\d+)?$/; 
-  if(compraregex.test(compra))
+  let regc=/^-?\d*\.?\d+$/; 
+  let compra=document.getElementById("ticket").value;
+  if(regc.test(compra))
   {
-    let descuento=parseFloat(compra)*0.15;
-    let preciofinal=parseFloat(compra)-descuento;
-    Problemas.getElementById("compraresultado").value=preciofinal;
+    let preciofinal=compra*0.85
+    document.getElementById("compraresultado").value=preciofinal;
   }
   else
   {
@@ -75,21 +74,17 @@ function descuento()
 function calis()
 {
   let calificacionregex=/^(10(\.0+)?|([0-9](\.[0-9]+)?))$/;
-  let evCont=Problemas.getElementById("calificacion1").value;
-  let eF=Problemas.getElementById("examenfinal").value;
-  let tF=Problemas.getElementById("trabajofinal").value;
-  if(calificacionregex.test(calificacion1))
+  let evCont=document.getElementById("calificacion1").value;
+  let eF=document.getElementById("examenfinal").value;
+  let tF=document.getElementById("trabajofinal").value;
+  if(calificacionregex.test(evCont))
   {
-    if(calificacionregex.test(calificacion2))
-    {
-      if(calificacionregex.test(calificacion3))
-      {
-        if (calificacionregex.test(califexamenfinal))
+        if (calificacionregex.test(eF))
         {
-          if(calificacionregex.test(trabajofinal))
+          if(calificacionregex.test(tF))
           {
             let porcentajefinal=(evCont*0.55)+(parseFloat(eF)*0.30)+(parseFloat(tF)*0.15);
-            Problemas.getElementById("calificacionfinal").value=porcentajefinal;
+            document.getElementById("calificacionfinal").value=porcentajefinal;
           }
           else
           {
@@ -105,58 +100,42 @@ function calis()
       {
         alert("Hay un valor incorrecto")
       }
-    }
-    else
-    {
-      alert("Hay un valor incorrecto")
-    }
-  }
-  else
-  {
-    alert("Hay un valor incorrecto")
-  }
-  
 }
 function porcentaje()
 {
   let regex1=/^[1-9]\d*$/;
-  let alH=Problemas.getElementById("alumnosh").value;
-  let alM=Problemas.getElementById("alumnosm").value;
-  if (regex1.test(numeroalumnos))
-  {
-    if (regex1.test(numeroalumnosh))
+  let alH=document.getElementById("alumnosh").value;
+  let alM=document.getElementById("alumnosm").value;
+  
+    if (regex1.test(alH))
     {
-      if (regex1.test(numeroalumnosm))
+      if (regex1.test(alM))
       {
         let totales=alH+alM
         let poralh=(alH*100)/totales;
         let poralm=(alM*100)/totales;
-        Problemas.getElementById("porcentajealumnos").value=poralh;
-        Problemas.getElementById("porcentajealumnas").value=poralm;
+        document.getElementById("porcentajealumnos").value=poralh;
+        document.getElementById("porcentajealumnas").value=poralm;
         }
       }
       else
       {
         alert("Hay un valor incorrecto")
       }
-  }
-  else
-  {
-    alert("Hay un valor incorrecto")
-  }
 }
 function edad() {
-  let fechaNacimiento = index.getElementById('')
-  let fechaNacimientoObj = new Date(fechaNacimiento);
-  let diferencia = Date.now() - fechaNacimientoObj.getTime();
+  let fechan=new Date(document.getElementById("fechan").value);
+  let diferencia = Date.now() - fechan.getTime();
   let edad = Math.floor(diferencia / 1000 / 60 / 60 / 24 / 365.25);
+
+  document.getElementById('edadc').value=edad
 }
 
 function numSuma()
 {
   let regc=/^-?\d*\.?\d+$/;
-  let n1=parseFloat(Problemas.getElementById("n1").value);
-  let n2=parseFloat(Problemas.getElementById("n2").value);
+  let n1=parseFloat(document.getElementById("n1").value);
+  let n2=parseFloat(document.getElementById("n2").value);
   let res;
   if(regc.test(n1))
   {
@@ -164,15 +143,15 @@ function numSuma()
     {
       if(n1==n2)
       {
-        resuk=n1*n2;
+        res=n1*n2;
       }
       if (n1>n2)
         res=n1-n2;
       else
       {
-        resuk=n1+n2;
+        res=n1+n2;
       }
-      Problemas.getElementById("resuk").value=res;
+      document.getElementById("resuk").value=res;
     }
     else
     {
@@ -187,9 +166,9 @@ function numSuma()
 function numMa()
 {
   let regd=/^-?\d*\.?\d+$/;
-  let num1=parseFloat(Problemas.getElementById("nu1").value);
-  let num2=parseFloat(Problemas.getElementById("nu2").value);
-  let num3=parseFloat(Problemas.getElementById("nu3").value);
+  let num1=parseFloat(document.getElementById("nu1").value);
+  let num2=parseFloat(document.getElementById("nu2").value);
+  let num3=parseFloat(document.getElementById("nu3").value);
   let mayor;
   if(regd.test(num1))
   {
@@ -197,7 +176,7 @@ function numMa()
     {
       if(regd.test(num3))
       {
-        if (num1>nume2) 
+        if (num1>num2) 
         {
           if (num1>num3)
           {
@@ -215,7 +194,7 @@ function numMa()
             mayor=num3
           }
         }
-        Problemas.getElementById("resup").value=mayor;
+        document.getElementById("resup").value=mayor;
       }
       else{
         alert("Hay un valor incorrecto")
@@ -232,8 +211,8 @@ function numMa()
 }
 function extras()
 {
-  let horasTrabajadas = Problemas.getElementById('horastotales').value
-  let sueldoPorHora = Problemas.getElementById('salario').value  
+  let horasTrabajadas = document.getElementById('horastotales').value
+  let sueldoPorHora = document.getElementById('salario').value  
   let rege1=/^\d+$/;
   let rege2=/^\d*\.?\d+$/;
   if(rege1.test(horasTrabajadas))
@@ -255,7 +234,7 @@ function extras()
           let sueldoExtrasAdicionales = horasExtrasAdicionales * sueldoPorHora * 3;
           sueldoBase += sueldoExtrasAdicionales;
       }
-      Problemas.getElementById("salariofinal").value=sueldoBase;
+      document.getElementById("salariofinal").value=sueldoBase;
     }
     else
     {
@@ -268,8 +247,8 @@ function extras()
 }  
 function repart()
 {
-  let año=index.getElementById('')
-  let suerox=index.getElementById('')
+  let año=document.getElementById('')
+  let suerox=document.getElementById('')
   let sueT
   if (año<1)
   {
@@ -291,5 +270,5 @@ function repart()
   {
       sueT=suerox*0.2
   }
-  Problemas.getElementById('sutilidad').value=sueT
+  document.getElementById('sutilidad').value=sueT
 }
